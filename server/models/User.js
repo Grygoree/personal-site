c04326c90f2a7420ffee509d47a8503e1e09d079
Grygoree <GregoryLeung2016@u.northwestern.edu>
2019-02-19 01:48:02 -0600

    Remove follow/follower methods and properties
    Following is not a feature of this blog

--- server/models/User.js
+++ server/models/User.js
@@ -7,31 +7,8 @@ let UserSchema = new mongoose.Schema(
     provider: String,
     provider_id: String,
     token: String,
-    provider_pic: String,
-    followers: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-      }
-    ],
-    following: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-      }
-    ]
+    provider_pic: String
   }
 )
-
-UserSchema.methods.follow = function(user_id) {
-  if(this.following.indexOf(user_id) === -1) {
-    this.following.push(user_id)
-  }
-  return this.save()
-}
-
-UserSchema.methods.addFollower = function(fs) {
-  this.followers.push(fs)
-}
 
 module.exports = mongoose.model('User', UserSchema)
